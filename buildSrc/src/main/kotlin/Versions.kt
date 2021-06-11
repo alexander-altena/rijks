084ac6d356bc52e92c12fe2c485156bb8c1fc67b
alexander-altena <alexander_altena@hotmail.com>
2021-06-11 15:22:49 +0200

    init: Setup common dependencies for ViewModel, Lifecycle, Room, DaggerHilt  Retrofit + Moshi which are needed in more than one module. Therefore the dependencies are placed in the App module

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,4 +1,31 @@
 object Versions {
+    //Gradle versions
     const val BUILD_TOOLS_VERSION = "4.2.1"
     const val KOTLIN_VERSION = "1.5.10"
+
+    //Core
+    const val ANDROID_MATERIAL_VERSION = "1.3.0"
+    const val ANDROID_X_FRAGMENT_KTX_VERSION = "1.3.4"
+    const val ANDROID_X_APPCOMPAT_VERSION = "1.3.0"
+    const val ANDROID_X_LEGACY_VERSION = "1.0.0"
+
+    //ViewModel
+    const val ANDROID_X_LIFECYCLE_VERSION = "2.3.1"
+
+    //Room
+    const val ANDROID_X_ROOM_VERSION = "2.3.0"
+
+    //Paging
+    const val ANDROID_X_PAGING_VERSION = "3.0.0"
+
+    //Coroutines
+    const val KOTLIN_COROUTINES_VERSION = "1.5.0"
+
+    //Network
+    const val MOSHI_VERSION = "1.12.0"
+    const val RETROFIT_VERSION = "2.9.0"
+
+    //DI
+    const val DAGGER_HILT_VERSION = "2.35"
+
 }
